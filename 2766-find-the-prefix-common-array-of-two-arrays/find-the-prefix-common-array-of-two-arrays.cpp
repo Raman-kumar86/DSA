e6af2fe052214a9b5410686367de65f7f6121e7d
Raman kumar <146643597+Raman-kumar86@users.noreply.github.com>
2026-05-20 13:57:24 +0530

    Time: 15 ms (30.66%) | Memory: 88.1 MB (36.86%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int idx1 = 0;
-        int idx2 = 0;
-        int num = 0;
-        while(idx1<n1 && idx2<n2){
-            if(nums1[idx1]==nums2[idx2]) {
-                num++;
-                idx1++;
-                idx2++;
-            }
-            else if(nums1[idx1]<nums2[idx2]) idx1++;
-            else idx2++;
-        }
-        return num;
-    }
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        unordered_map<int, int>freq;
         int n = A.size();
         vector<int>result(n);
+        int common = 0;
         for(int i=0;i<n;i++){
-            vector<int>nums1,nums2;
-            nums1.assign(A.begin(), A.begin()+i+1);
-            nums2.assign(B.begin(), B.begin()+i+1);
-            result[i] = getCommon(nums1, nums2);
+            freq[A[i]]++;
+            if(freq[A[i]]==2) common++;
+            freq[B[i]]++;
+            if(freq[B[i]]==2) common++;
+            result[i] = common;
         }
         return result;
     }
