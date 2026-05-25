ecbb78e47c8d9ac178b21165d9675bdc833e8a2d
Raman kumar <146643597+Raman-kumar86@users.noreply.github.com>
2026-05-25 22:34:37 +0530

    Time: 48 ms (16.38%) | Memory: 53.2 MB (15.90%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int n;
-    int arr[10001];
-    bool recurr(int idx, vector<int>&nums){
-        if(idx>=n-1) return true;
-        if(arr[idx]!=-1) return arr[idx];
-        for(int i=1;i<=nums[idx];i++){
-            if(recurr(idx+i,nums)) return arr[idx] = true;
-        }
-        return arr[idx] = false;
-    }
     bool canJump(vector<int>& nums) {
-        n = nums.size();
-        memset(arr, -1, sizeof(arr));
-        return recurr(0, nums);
+        int n = nums.size();
+        vector<bool>arr(n, false);
+        arr[0] = true;
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(arr[j] && j+nums[j]>=i){
+                    arr[i] = true;
+                    break;
+                }
+            }
+        }
+        return arr[n-1];
     }
 };
