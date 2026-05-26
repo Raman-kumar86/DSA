2fa9ed15032715ff4da86faacb22cdc0ebef035a
Raman kumar <146643597+Raman-kumar86@users.noreply.github.com>
2026-05-27 00:42:52 +0530

    Time: 96 ms (39.54%) | Memory: 81 MB (32.88%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.cpp
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -2,38 +2,39 @@ class Solution {
 public:
     int minJumps(vector<int>& arr) {
         int n = arr.size();
-        int steps = 0;
-        unordered_map<int,vector<int>>um;
         vector<bool>visited(n, false);
+        queue<int>q;
+        q.push(0);
+        visited[0] = true;
+        unordered_map<int,vector<int>>um;
         for(int i=0;i<n;i++){
             um[arr[i]].push_back(i);
         }
-        queue<int>q;
-        q.push(0);
+        int steps = 0;
         while(!q.empty()){
             int len = q.size();
             while(len--){
                 int idx = q.front();
-                q.pop();
                 if(idx==n-1) return steps;
-                if(idx-1>=0 && !visited[idx-1]){
-                    visited[idx-1] = true;
-                    q.push(idx-1);
-                }
+                q.pop();
                 if(idx+1<n && !visited[idx+1]){
                     visited[idx+1] = true;
                     q.push(idx+1);
                 }
-                for(int i=0;i<um[arr[idx]].size();i++){
-                    if(!visited[um[arr[idx]][i]]){
-                        visited[um[arr[idx]][i]] = true;
-                        q.push(um[arr[idx]][i]);
+                if(idx-1>=0 && !visited[idx-1]){
+                    visited[idx-1] = true;
+                    q.push(idx-1);
+                }
+                vector<int>sameVal = um[arr[idx]];
+                for(int i=0;i<sameVal.size();i++){
+                    if(!visited[sameVal[i]]){
+                        q.push(sameVal[i]);
                     }
                 }
                 um[arr[idx]].clear();
             }
             steps++;
         }
-        return -1;
+        return 0;
     }
 };
