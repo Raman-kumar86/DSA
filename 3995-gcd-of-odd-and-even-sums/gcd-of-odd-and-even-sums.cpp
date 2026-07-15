5d48f536a70ccca094b598fcb896bb5d40451fcf
Raman kumar <146643597+Raman-kumar86@users.noreply.github.com>
2026-07-15 14:20:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (99.40%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
-    int gcd(int x, int y){
-        while(x!=y){
-            if(x>y){
-                x = x - y;
-            }else if(x<y){
-                y = y-x;
-            }
-        }
-        return x;
-    }
     int gcdOfOddEvenSums(int n) {
-        int even_sum = n*(n+1);
-        int odd_sum = n*n;
-        return gcd(even_sum, odd_sum);
+        return n;
     }
 };
