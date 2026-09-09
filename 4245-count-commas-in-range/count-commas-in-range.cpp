7eb778e118fbe9a56d5216e0b52766ecdd166862
Raman kumar <146643597+Raman-kumar86@users.noreply.github.com>
2026-09-09 22:31:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (5.41%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        string str1 = to_string(n);
+        int len = str1.size();
+        if(len<4) return 0;
+        return n-999;
+    }
+};
